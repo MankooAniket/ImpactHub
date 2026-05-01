7435e5826bf6f3f2a7fd89ce920e26b3bbe42ef8
MankooAniket <amankoo2004@gmail.com>
2026-05-02 03:07:09 +0530

    fixed token generation

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -13,11 +13,11 @@ const generateToken = (
     { expiresIn: '7d' }
   );
 
-  res.cookie('jwt', token, {
+  res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
   return token;
